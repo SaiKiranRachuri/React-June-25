478074450be35b2f47505aa37f55f80333b0edfc
SAI KIRAN <saikirankemal@gmail.com>
2025-06-21 09:37:30 +0530

    Updated app.js

---
+++ 07-accordion-component-v1/src/App.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import "./index.css";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Accordion />
+    </div>
+  );
+}
+
+function Accordion() {
+  return (
+    <div className="accordion">
+      {faqs.map((faq, i) => (
+        <AccordionItem title={faq.title} text={faq.text} num={i + 1} />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ title, text, num }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+  return (
+    <div className={isOpen ? "item open" : "item"} onClick={handleToggle}>
+      <p className={isOpen ? "number open" : "number"}>
+        {num < 9 ? `0${num}` : num}
+      </p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
+    </div>
+  );
+}
