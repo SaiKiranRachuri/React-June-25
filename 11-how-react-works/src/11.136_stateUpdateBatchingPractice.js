946db5574fb81be2565d05f96222391968dc410c
SAI KIRAN <saikirankemal@gmail.com>
2025-07-30 10:15:16 +0530

    added file 11.136

---
+++ 11-how-react-works/src/11.136_stateUpdateBatchingPractice.js
@@ -0,0 +1,134 @@
+//// 1)Prove the state updates are batched by showing the component is rendered only once.
+// Ideally as the event handler has 2 statements the component should be rendered twice however, as the state updates are batched the component is rendered once as can be seen by the log statement: line 81 to 84.
+
+//// 2) Prove state updates are Asynchronous by logging the likes
+// Once after we update the likes on page and we click on undo button as per the previous statement in event handler the likes should be zero. However, it displays the current state of fiber tree. line 85.
+
+//// 3) When state values are already to default, button click will NOT rerender the component. i.e., current state === new state. Notice it doesn't log the "Render Check" statement.
+
+//// 4) Handling Triple Increment of likes doesn't work correct due to the same Asynchronous state updates. The second statement of state update doesn't know the current state update valuea and stil refers to previous. To make it work always use the callback function. lines 89 to 92.
+
+//// 5) To prove auto batching works in React18 outside event handlers i.e., setTimeOuts, promises, native event handlers. Component is rendered only once despite multiple statements. line 100.
+
+import { useState } from "react";
+
+const content = [
+  {
+    summary: "React is a library for building UIs",
+    details:
+      "Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    summary: "State management is like giving state a home",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    summary: "We can think of props as the component API",
+    details:
+      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Tabbed content={content} />
+    </div>
+  );
+}
+
+function Tabbed({ content }) {
+  const [activeTab, setActiveTab] = useState(0);
+
+  return (
+    <div>
+      <div className="tabs">
+        <Tab num={0} activeTab={activeTab} onClick={setActiveTab} />
+        <Tab num={1} activeTab={activeTab} onClick={setActiveTab} />
+        <Tab num={2} activeTab={activeTab} onClick={setActiveTab} />
+        <Tab num={3} activeTab={activeTab} onClick={setActiveTab} />
+      </div>
+
+      {activeTab <= 2 ? (
+        <TabContent item={content.at(activeTab)} />
+      ) : (
+        <DifferentContent />
+      )}
+    </div>
+  );
+}
+
+function Tab({ num, activeTab, onClick }) {
+  return (
+    <button
+      className={activeTab === num ? "tab active" : "tab"}
+      onClick={() => onClick(num)}
+    >
+      Tab {num + 1}
+    </button>
+  );
+}
+
+function TabContent({ item }) {
+  const [showDetails, setShowDetails] = useState(true);
+  const [likes, setLikes] = useState(0);
+
+  function handleInc() {
+    setLikes(likes + 1);
+  }
+
+  console.log("Render check");
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+    console.log(likes);
+  }
+
+  function hanldeTripleInc() {
+    // setLikes(likes + 1);
+    // console.log(likes);
+    // setLikes(likes + 1);
+    // setLikes(likes + 1);
+
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+  }
+
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
+  }
+
+  return (
+    <div className="tab-content">
+      <h4>{item.summary}</h4>
+      {showDetails && <p>{item.details}</p>}
+
+      <div className="tab-actions">
+        <button onClick={() => setShowDetails((h) => !h)}>
+          {showDetails ? "Hide" : "Show"} details
+        </button>
+
+        <div className="hearts-counter">
+          <span>{likes} ❤️</span>
+          <button onClick={handleInc}>+</button>
+          <button onClick={hanldeTripleInc}>+++</button>
+        </div>
+      </div>
+
+      <div className="tab-undo">
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
+      </div>
+    </div>
+  );
+}
+
+function DifferentContent() {
+  return (
+    <div className="tab-content">
+      <h4>I'm a DIFFERENT tab, so I reset state 💣💥</h4>
+    </div>
+  );
+}
--- 11-how-react-works/src/App.js
+++ 11-how-react-works/src/App.js
@@ -1,30 +1,134 @@
-//// Batching of multiple setState calls in event handlers
-// Here below setAnswer, console.log, setSolved are not updated line by line. setAnser and setSolved are sent together as a batch and are updated.
-// The value of answer in console.log is the current state value and NOT the updated value. Because in fiber tree the current state is stored and only fiber tree is updated after rerender is done.
-// State updates are Asychronous.
+//// 1)Prove the state updates are batched by showing the component is rendered only once.
+// Ideally as the event handler has 2 statements the component should be rendered twice however, as the state updates are batched the component is rendered once as can be seen by the log statement: line 81 to 84.
 
-// If we need to update state based on previous update then we use setState with a call back function.
+//// 2) Prove state updates are Asynchronous by logging the likes
+// Once after we update the likes on page and we click on undo button as per the previous statement in event handler the likes should be zero. However, it displays the current state of fiber tree. line 85.
 
-// This automatic batching is done in Event handlers on React17 and React 18. However, in timeOuts, promises and native events(like addEventListener) in React17 auto batching is NOT done and state updates are done line by line.
+//// 3) When state values are already to default, button click will NOT rerender the component. i.e., current state === new state. Notice it doesn't log the "Render Check" statement.
 
-// In situations where auto batching is problematic and to STOP it use
-// wrap the state update in ReactDOM.flushSync()
+//// 4) Handling Triple Increment of likes doesn't work correct due to the same Asynchronous state updates. The second statement of state update doesn't know the current state update valuea and stil refers to previous. To make it work always use the callback function. lines 89 to 92.
+
+//// 5) To prove auto batching works in React18 outside event handlers i.e., setTimeOuts, promises, native event handlers. Component is rendered only once despite multiple statements. line 100.
 
 import { useState } from "react";
 
-export default function App() {
-  const [answer, setAnswer] = useState("test");
-  const [solved, setSolved] = useState("test");
+const content = [
+  {
+    summary: "React is a library for building UIs",
+    details:
+      "Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    summary: "State management is like giving state a home",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    summary: "We can think of props as the component API",
+    details:
+      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  },
+];
 
-  const reset = function () {
-    setAnswer(" ");
-    console.log(answer);
-    setSolved(" ");
-  };
+export default function App() {
+  return (
+    <div>
+      <Tabbed content={content} />
+    </div>
+  );
+}
+
+function Tabbed({ content }) {
+  const [activeTab, setActiveTab] = useState(0);
 
   return (
     <div>
-      <button onClick={reset}></button>
+      <div className="tabs">
+        <Tab num={0} activeTab={activeTab} onClick={setActiveTab} />
+        <Tab num={1} activeTab={activeTab} onClick={setActiveTab} />
+        <Tab num={2} activeTab={activeTab} onClick={setActiveTab} />
+        <Tab num={3} activeTab={activeTab} onClick={setActiveTab} />
+      </div>
+
+      {activeTab <= 2 ? (
+        <TabContent item={content.at(activeTab)} />
+      ) : (
+        <DifferentContent />
+      )}
+    </div>
+  );
+}
+
+function Tab({ num, activeTab, onClick }) {
+  return (
+    <button
+      className={activeTab === num ? "tab active" : "tab"}
+      onClick={() => onClick(num)}
+    >
+      Tab {num + 1}
+    </button>
+  );
+}
+
+function TabContent({ item }) {
+  const [showDetails, setShowDetails] = useState(true);
+  const [likes, setLikes] = useState(0);
+
+  function handleInc() {
+    setLikes(likes + 1);
+  }
+
+  console.log("Render check");
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+    console.log(likes);
+  }
+
+  function hanldeTripleInc() {
+    // setLikes(likes + 1);
+    // console.log(likes);
+    // setLikes(likes + 1);
+    // setLikes(likes + 1);
+
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+  }
+
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
+  }
+
+  return (
+    <div className="tab-content">
+      <h4>{item.summary}</h4>
+      {showDetails && <p>{item.details}</p>}
+
+      <div className="tab-actions">
+        <button onClick={() => setShowDetails((h) => !h)}>
+          {showDetails ? "Hide" : "Show"} details
+        </button>
+
+        <div className="hearts-counter">
+          <span>{likes} ❤️</span>
+          <button onClick={handleInc}>+</button>
+          <button onClick={hanldeTripleInc}>+++</button>
+        </div>
+      </div>
+
+      <div className="tab-undo">
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
+      </div>
+    </div>
+  );
+}
+
+function DifferentContent() {
+  return (
+    <div className="tab-content">
+      <h4>I'm a DIFFERENT tab, so I reset state 💣💥</h4>
     </div>
   );
 }
