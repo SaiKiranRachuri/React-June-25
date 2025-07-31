c7d6536e3c8643e1416bebebe5df5c344f85cd37
SAI KIRAN <saikirankemal@gmail.com>
2025-07-31 19:27:46 +0530

    added files 12.147

--- 10-usepopcorn/src/index.js
+++ 10-usepopcorn/src/index.js
@@ -7,8 +7,9 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
+    {/* <StarRating maxRating={5} />
     <StarRating color="red" size={20} className="test" />
-    <StarRating />
+    <StarRating /> */}
+    <App />
   </React.StrictMode>
 );
