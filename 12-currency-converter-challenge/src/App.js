86cfeaad0619ab604ac72c85dbe0e25c5acd953e
SAI KIRAN <saikirankemal@gmail.com>
2025-09-09 11:59:18 +0530

    added files 12-currency-converter

---
+++ 12-currency-converter-challenge/src/App.js
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [amount, setAmount] = useState(1);
+  const [fromCur, setFromCur] = useState("USD");
+  const [toCur, setToCur] = useState("EUR");
+  const [converted, setConverted] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(
+    function () {
+      async function convert() {
+        setIsLoading(true);
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`
+        );
+        const data = await res.json();
+        console.log(data);
+        setConverted(data.rates[toCur]);
+        setIsLoading(false);
+      }
+
+      if (fromCur === toCur) {
+        return setConverted(amount);
+      }
+
+      convert();
+    },
+    [amount, fromCur, toCur]
+  );
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={amount}
+        disabled={isLoading}
+        onChange={(e) => {
+          setAmount(Number(e.target.value));
+        }}
+      />
+      <select
+        value={fromCur}
+        disabled={isLoading}
+        onChange={(e) => setFromCur(e.target.value)}
+      >
+        <option value="USD">USD</option>
+        <option value="CAD">CAD</option>
+        <option value="EUR">EUR</option>
+        <option value="INR">INR</option>
+      </select>
+      <select
+        value={toCur}
+        disabled={isLoading}
+        onChange={(e) => setToCur(e.target.value)}
+      >
+        <option value="USD">USD</option>
+        <option value="CAD">CAD</option>
+        <option value="EUR">EUR</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>
+        {converted} {toCur}
+      </p>
+    </div>
+  );
+}
