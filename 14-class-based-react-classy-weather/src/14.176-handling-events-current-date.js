1637c730fbdb28a4716699822adc2b2fe2d0b48c
SAI KIRAN <saikirankemal@gmail.com>
2025-10-03 19:16:39 +0530

    added files 14.176 and 14.175

---
+++ 14-class-based-react-classy-weather/src/14.176-handling-events-current-date.js
@@ -0,0 +1,45 @@
+// Handling events in classy way
+// Setting state
+// Display date
+
+import React from "react";
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { count: 5 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleDecrement() {
+    console.log(this);
+    this.setState((curState) => {
+      return { count: curState.count - 1 };
+    });
+  }
+
+  handleIncrement() {
+    console.log(this);
+    this.setState((curState) => {
+      return { count: curState.count + 1 };
+    });
+  }
+
+  render() {
+    const date = new Date("October 3 2025");
+    date.setDate(date.getDate() + this.state.count);
+    return (
+      <div>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>
+          {date.toDateString()}[{this.state.count}]
+        </span>
+        <button onClick={this.handleIncrement}>+</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
