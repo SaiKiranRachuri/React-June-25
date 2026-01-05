593ef7de243dbb30abc746cfca7cb4bacc128075
SAI KIRAN <saikirankemal@gmail.com>
2026-01-05 19:19:05 +0530

    added file 18.237 Citiescontext

---
+++ 18-worldwise-copy-context-api/contexts/CitiesContext original v1.jsx
@@ -0,0 +1,106 @@
+import { createContext, useState, useEffect, useContext } from "react";
+
+const CitiesContext = createContext();
+
+const BASE_URL = "http://localhost:9000";
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        // console.log("Res:", res);
+        const data = await res.json();
+        // console.log("Data:", data);
+        setCities(data);
+      } catch {
+        alert("There was an error fetching data.");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      // console.log("Res:", res);
+      const data = await res.json();
+      // console.log("Data:", data);
+      setCurrentCity(data);
+    } catch {
+      alert("There was an error fetching data.");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function createCity(newCity) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities`, {
+        method: "POST",
+        body: JSON.stringify(newCity),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      // console.log("Res:", res);
+      const data = await res.json();
+      console.log("From context: New City Data:", data);
+
+      setCities((cities) => [...cities, data]);
+    } catch {
+      alert("There was an error fetching data.");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE",
+      });
+
+      setCities(cities.filter((city) => city.id !== id));
+    } catch {
+      alert("There was an error deleting data.");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+        getCity,
+        currentCity,
+        createCity,
+        deleteCity,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw Error("You are trying to access context out of its provider.");
+  return context;
+}
+
+// export { CitiesContext, CitiesProvider };
+export { useCities, CitiesProvider };
